69aa07416ee4974d4188d3aa568ddf13c217e8c8
Kenny <high0048@126.com>
2025-07-05 02:26:42 +0800

    update 6.1

--- src/main/scala/scalawithcats/ch6/SemigroupalExample.scala
+++ src/main/scala/scalawithcats/ch6/SemigroupalExample.scala
@@ -19,4 +19,20 @@ object SemigroupalExample extends App {
   val res3 = Semigroupal[Option].product(Some(123), None)
   // res3: Option[Tuple2[Int, Nothing]] = None
   p1(res3)
+
+  import cats.instances.option._ // for Semigroupal
+  val res4 = Semigroupal.tuple3(Option(1), Option(2), Option(3))
+  // res4: Option[(Int, Int, Int)] = Some((1, 2, 3))
+  p1(res4)
+  val res5 = Semigroupal.tuple3(Option(1), Option(2), Option.empty[Int])
+  // res5: Option[(Int, Int, Int)] = None
+  p1(res5)
+  val res6 = Semigroupal.map3(Option(1), Option(2), Option(3))(_ + _ + _)
+  // res6: Option[Int] = Some(6)
+  p1(res6)
+  val res7 = Semigroupal.map2(Option(1), Option.empty[Int])(_ + _)
+  // res7: Option[Int] = None
+  p1(res7)
+
+//  product(a, product(b, c)) == product(product(a, b), c)
 }
