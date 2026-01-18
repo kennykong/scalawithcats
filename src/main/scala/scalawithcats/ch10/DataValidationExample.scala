f0b20d4e6d57ceda49682ac01b78243c74215e45
kenny <koxy2008@gmail.com>
2026-01-19 00:50:23 +0800

    update 10.4.2

--- src/main/scala/scalawithcats/ch10/DataValidationExample.scala
+++ src/main/scala/scalawithcats/ch10/DataValidationExample.scala
@@ -122,6 +122,11 @@ object DataValidationExample extends App {
   val check2: Check[List[String], Int] =
     a2 and b2
 
+  val res7 = check2(5)
+  p(res7)
+
+  val res8 = check2(0)
+  p(res8)
 
   //Second ADT Check
 
@@ -201,4 +206,96 @@ object DataValidationExample extends App {
     final case class Pure[E, A](func: A => Validated[E, A]) extends Check3[E, A]
   }
 
+  // 10.4 Transforming Data
+
+  import cats.Semigroup
+  import cats.data.Validated
+  import cats.syntax.semigroup._ // for |+|
+  import cats.syntax.apply._
+  // for mapN
+  import cats.data.Validated._
+
+  // for Valid and Invalid
+  sealed trait Predicate[E, A] {
+
+    import Predicate._
+
+    def and(that: Predicate[E, A]): Predicate[E, A] =
+      And(this, that)
+
+    def or(that: Predicate[E, A]): Predicate[E, A] =
+      Or(this, that)
+
+    def apply(a: A)(implicit s: Semigroup[E]): Validated[E, A] =
+      this match {
+        case Pure(func) =>
+          func(a)
+        case And(left, right) =>
+          (left(a), right(a)).mapN((_, _) => a)
+        case Or(left, right) =>
+          left(a) match {
+            case Valid(_) => Valid(a)
+            case Invalid(e1) =>
+              right(a) match {
+                case Valid(_) => Valid(a)
+                case Invalid(e2) => Invalid(e1 |+| e2)
+              }
+          }
+      }
+  }
+
+  object Predicate {
+    final case class And[E, A](left: Predicate[E, A],
+                               right: Predicate[E, A]) extends Predicate[E, A]
+
+    final case class Or[E, A](left: Predicate[E, A],
+                              right: Predicate[E, A]) extends Predicate[E, A]
+
+    final case class Pure[E, A](func: A => Validated[E, A]) extends Predicate[E, A]
+  }
+
+  import cats.Semigroup
+  import cats.data.Validated
+
+  sealed trait Check4[E, A, B] {
+
+    import Check4._
+
+    def apply(in: A)(implicit s: Semigroup[E]): Validated[E, B]
+
+    def map[C](func: B => C): Check4[E, A, C] = Map[E, A, B, C](this, func)
+
+    def flatMap[C](f: B => Check4[E, A, C]) = FlatMap[E, A, B, C](this, f)
+
+    def andThen[C](that: Check4[E, B, C]): Check4[E, A, C] = AndThen(this, that)
+  }
+
+  object Check4 {
+    final case class Map[E, A, B, C](check: Check4[E, A, B],
+                                     func: B => C) extends Check4[E, A, C] {
+      def apply(in: A)(implicit s: Semigroup[E]): Validated[E, C] =
+        check(in).map(func)
+    }
+
+    final case class Pure[E, A](pred: Predicate[E, A]) extends Check4[E, A, A] {
+      def apply(in: A)(implicit s: Semigroup[E]): Validated[E, A] =
+        pred(in)
+    }
+
+    def apply[E, A](pred: Predicate[E, A]): Check4[E, A, A] = Pure(pred)
+
+    final case class FlatMap[E, A, B, C](check: Check4[E, A, B],
+                                         func: B => Check4[E, A, C]) extends Check4[E, A, C] {
+      def apply(a: A)(implicit s: Semigroup[E]): Validated[E, C] =
+        check(a).withEither(_.flatMap(b => func(b)(a).toEither))
+
+    }
+
+    final case class AndThen[E, A, B, C](check1: Check4[E, A, B],
+                                         check2: Check4[E, B, C]) extends Check4[E, A, C] {
+      override def apply(a: A)(implicit s: Semigroup[E]): Validated[E, C] =
+        check1(a).withEither(_.flatMap(b => check2(b).toEither))
+    }
+
+  }
 }
