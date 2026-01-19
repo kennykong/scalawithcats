e1ba9cefc2a24c5807dffcc7050364acf470ffb7
kenny <koxy2008@gmail.com>
2026-01-20 02:25:14 +0800

    update 10.5

--- src/main/scala/scalawithcats/ch10/KleisliExample.scala
+++ src/main/scala/scalawithcats/ch10/KleisliExample.scala
@@ -14,4 +14,16 @@ object KleisliExample extends App{
     Kleisli(x => List(x * 2, x / 2))
 
   val pipeline = step1 andThen step2 andThen step3
+  val res0 = pipeline.run(20)
+  p(res0)
+
+  import cats.Semigroup
+  import cats.data.Validated
+  sealed trait Predicate[E, A] {
+    def run(implicit s: Semigroup[E]): A => Either[E, A] =
+      (a: A) => this(a).toEither
+    def apply(a: A): Validated[E, A] =
+      ??? // etc...
+    // other methods...
+  }
 }
